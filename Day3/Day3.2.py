be1fd3ab899ff84e6980d48e6df7edcb07f9a48e
Luca Giuberti <gix_lg@yahoo.com>
2019-12-04 12:57:36 +0100

    Solved Day3 - part 2

---
+++ Day3/Day3.2.py
@@ -0,0 +1,69 @@
+def get_points_from_moves(moves):
+    points = []
+    previous_point = (0, 0)
+    points.append(previous_point)
+    for move in moves:
+        direction = move[0:1]
+        step = int(move[1:])
+        if direction == "U":
+            new_point = [(previous_point[0], previous_point[1] + (inc + 1)) for inc in range(step)]
+        if direction == "D":
+            new_point = [(previous_point[0], previous_point[1] - (inc + 1)) for inc in range(step)]
+        if direction == "R":
+            new_point = [(previous_point[0] + (inc + 1), previous_point[1]) for inc in range(step)]
+        if direction == "L":
+            new_point = [(previous_point[0] - (inc + 1), previous_point[1]) for inc in range(step)]
+        points.extend(new_point)
+        previous_point = points[-1]
+
+    return points
+
+
+def print_points(points1, points2):
+    # importing two required module
+    import numpy as np
+    import matplotlib.pyplot as plt
+    # Creating a numpy array
+    X1 = np.array([x for (x, y) in points1])
+    Y1 = np.array([y for (x, y) in points1])
+    # Plotting point using sactter method
+    plt.scatter(X1, Y1)
+
+    X2 = np.array([x for (x, y) in points2])
+    Y2 = np.array([y for (x, y) in points2])
+    # Plotting point using sactter method
+    plt.scatter(X2, Y2)
+    plt.show()
+
+
+def find_cross_points(points1, points2):
+    a_set = set(points1)
+    b_set = set(points2)
+    if a_set & b_set:
+        cross_points = list(a_set & b_set)
+        cross_points.remove((0, 0))
+        return cross_points
+
+
+def distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+with open("inputDay3.txt") as f:
+    moves = f.readline().split(",")
+    points1 = get_points_from_moves(moves)
+    print(points1)
+
+    moves = f.readline().split(",")
+    points2 = get_points_from_moves(moves)
+    print(points2)
+
+    cross = find_cross_points(points1, points2)
+    distances = [distance((0, 0), p) for p in cross]
+    print(f"cross: {cross}")
+    print(f"distances: {distances}")
+    print(f"min(distances): {min(distances)}")
+
+    sums_of_steps = [points1.index(c) + points2.index(c) for c in cross]
+    print(f"min(sums_of_steps): {min(sums_of_steps)}")
+    #print_points(points1, points2)
