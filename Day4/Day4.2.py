706d667d20118f9ce697af1239d66be2e17cadc2
Luca Giuberti <gix_lg@yahoo.com>
2019-12-05 21:21:47 +0100

    Solved Day4 - part 2 - [Possible imporvement: add tests]

--- Day4/Day4.2.py
+++ Day4/Day4.2.py
@@ -3,17 +3,31 @@ def validate_password(password):
 
 
 def there_are_2_adjacent_digits(password):
-    for i in range(len(password) - 1 ):
-        if password[i] == password[i +1]:
-            return True
-    return False
+    count = 0
+    for i in range(len(password) - 1):
+        if password[i] == password[i + 1]:
+            count += 1
+        else:
+            if count == 1:
+                return True
+            count = 0
+    return count == 1
 
 
 def is_digits_increase_from_left_to_right(password):
-    for i in range(len(password) - 1 ):
-        if password[i] > password[i +1]:
+    for i in range(len(password) - 1):
+        if password[i] > password[i + 1]:
             return False
     return True
+
+
+p1 = "112233"
+p2 = "123444"
+p3 = "111122"
+
+print(f"password: {p1}  {validate_password(p1)}")
+print(f"password: {p2}  {validate_password(p2)}")
+print(f"password: {p3}  {validate_password(p3)}")
 
 
 with open("inputDay4.txt") as f:
@@ -24,12 +38,3 @@ with open("inputDay4.txt") as f:
         if validate_password(password):
             count += 1
     print(f"Valid passwords in the range are : {count}")
-
-
-# p1 = "111111"
-# p2 = "223450"
-# p3 = "123789"
-#
-# print(f"password: {p1}  {validate_password(p1)}")
-# print(f"password: {p2}  {validate_password(p2)}")
-# print(f"password: {p3}  {validate_password(p3)}")
